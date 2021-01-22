f2b16e5194597d037e0b18a73730943462022cb2
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-01-22 16:15:43 -0500

    commit tree 6fe834f775d904dafe64f4eea8ebadf25581e7c2

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
 # code here!
+class School
+
+    attr_accessor  :roster
+    # attr_reader :name
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+     def add_student(student, grade)
+        if @roster[grade]
+            @roster[grade] << student
+          else
+            @roster[grade] = [student]
+          
+        end
+     end
+
+       def grade(grade)
+        @roster[grade]
+
+       end
+
+     def sort
+        sorted_roster = {}
+        @roster.sort.each{|grade, students| sorted_roster[grade] = students.sort }
+        sorted_roster
+     end
+
+end
